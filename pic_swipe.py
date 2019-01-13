5971799d5ef7388ecdea431ffa8038936351a807
MassimoCastell <Massimo.Castell@gmail.com>
2019-01-13 23:52:24 +0100

    Add files via upload
    Change to subroutines.
    Viewer windows with fixed height but using same picture ratio.

--- pic_swipe.py
+++ pic_swipe.py
@@ -14,46 +14,50 @@ root = tk.Tk()
 root.withdraw()
 file_path = os.path.dirname(filedialog.askopenfilename())
 
-# content of picutre folder
+# content of picture folder
 picturelist = os.listdir(file_path)
 index = 0
 
-# create viewer windows, resize picture
-cv2.namedWindow("image", cv2.WINDOW_NORMAL)
-img = cv2.imread(filename = str(file_path)+"/"+str(picturelist[index]))
-cv2.resizeWindow('image',800, 600)
-cv2.imshow('image',img)
+def viewpic(index):
+    # create viewer windows, resize picture
+    cv2.namedWindow("image", cv2.WINDOW_NORMAL)
+    img = cv2.imread(filename = str(file_path)+"/"+str(picturelist[index]))
+    h, w, r = 0, 0, 0
+    h, w, r = img.shape
+    ratio = w / h
+    height = 800
+    width = int(height * ratio)
+    cv2.resizeWindow('image',(width, height))
+    cv2.imshow('image',img)
 
-while(1):
-    k = cv2.waitKeyEx(0)
-    if k==27:           # wait for ESC key to exit
-        cv2.destroyAllWindows()
-        break
-    elif k==2490368:    # cursor up move to delete folder
-        shutil.move(str(file_path)+"/"+str(picturelist[index]), str(todelete)+"/"+str(picturelist[index]))
-        index += 1
-        img = cv2.imread(filename = str(file_path)+"/"+str(picturelist[index]))
-        cv2.resizeWindow('image',800, 600)
-        cv2.imshow('image',img)
-    elif k==2621440:    #cursor down move to sort folder
-        shutil.move(str(file_path)+"/"+str(picturelist[index]), str(sort)+"/"+str(picturelist[index]))
-        index += 1
-        img = cv2.imread(filename = str(file_path)+"/"+str(picturelist[index]))
-        cv2.resizeWindow('image',800, 600)
-        cv2.imshow('image',img)
-    elif k==2424832:    # cursor left previous picture
-        index -= 1
-        if index < 0:
-            index = 0
-        img = cv2.imread(filename = str(file_path)+"/"+str(picturelist[index]))
-        cv2.resizeWindow('image',800, 600)
-        cv2.imshow('image',img)
-    elif k==2555904:    # cursor right next picture
-        index += 1
-        img = cv2.imread(filename = str(file_path)+"/"+str(picturelist[index]))
-        cv2.resizeWindow('image',800, 600)
-        cv2.imshow('image',img)
-    elif k==-1:         # ignore waitkey standard input
-        continue
-    else:               # print every other pressed key
-        print(k)    
+def picloop():
+    global index
+    while(1):
+        k = cv2.waitKeyEx(0)
+        if k==27:           # wait for ESC key to exit
+            cv2.destroyAllWindows()
+            break
+        elif k==2490368:    # cursor up move to delete folder
+            shutil.move(str(file_path)+"/"+str(picturelist[index]), str(todelete)+"/"+str(picturelist[index]))
+            index += 1
+            viewpic(index)
+        elif k==2621440:    #cursor down move to sort folder
+            shutil.move(str(file_path)+"/"+str(picturelist[index]), str(sort)+"/"+str(picturelist[index]))
+            index += 1
+            viewpic(index)
+        elif k==2424832:    # cursor left previous picture
+            index -= 1
+            if index < 0:
+                index = 0
+            viewpic(index)
+        elif k==2555904:    # cursor right next picture
+            index += 1
+            viewpic(index)
+        elif k==-1:         # ignore waitkey standard input
+            continue
+        else:               # print every other pressed key
+            print(k)    
+
+if __name__ == '__main__':
+    viewpic(index)
+    picloop()
